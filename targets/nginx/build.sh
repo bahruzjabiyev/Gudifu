0765a9c5b1e5148ede2bfa548eb2a9edfe1e526d
Bahruz Jabiyev <behruzcebiyev@gmail.com>
2022-08-02 14:26:58 +0000

    Remove wrappers as it can interfere with the needed nginx behavior (e.g., forwarding)

--- targets/nginx/build.sh
+++ targets/nginx/build.sh
@@ -24,7 +24,6 @@ rm -rf genfiles && mkdir genfiles && $SRC/LPM/external.protobuf/bin/protoc http_
 cd ../..
 
 auto/configure \
-    --with-ld-opt="-Wl,--wrap=listen -Wl,--wrap=setsockopt -Wl,--wrap=bind -Wl,--wrap=shutdown -Wl,--wrap=connect -Wl,--wrap=getpwnam -Wl,--wrap=getgrnam -Wl,--wrap=chmod -Wl,--wrap=chown" \
     --with-cc-opt='-DNGX_DEBUG_PALLOC=1' \
     --with-http_v2_module 
 make -f objs/Makefile fuzzers
